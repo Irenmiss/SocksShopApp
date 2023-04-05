2cb7cde75bb0756cf00ea963fcc592c30c513c38
irina.kustova <irenekitty@yandex.ru>
2023-04-05 14:24:11 +0300

    initial commit

---
+++ src/test/java/learnjava/socksshopapp/socksshopapp/SocksShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package learnjava.socksshopapp.socksshopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksShopAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
